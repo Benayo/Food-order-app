9ed1a17186dba8be90e0d148c751b834900daa0b
Benayo <ben_odeleye@yahoo.com>
2022-08-11 21:02:42 +0100

    Dashboard

--- src/components/Cart/CartIcon.js
+++ src/components/Cart/CartIcon.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import classes from "./CartIcon.module.css";
 const CartIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className="h-5 w-5"
+      className={classes.icon}
       viewBox="0 0 20 20"
       fill="currentColor"
     >
