a26cb59548b0bd1c8df8000acd2d81ea5266be3b
Benayo <ben_odeleye@yahoo.com>
2022-09-21 17:50:22 +0100

    latest codes

--- src/pages/auth/SignIn.js
+++ src/pages/auth/SignIn.js
@@ -1,11 +1,8 @@
 import React, { useState } from "react";
-// import { useHistory } from "react-router-dom";
 import axios from "axios";
 
 import useInput from "../../hook/use-input";
-
 import classes from "./SignIn.module.css";
-// import AuthContext from "../../store/auth-context";
 import SignInNav from "../../layout/Navigation/SignInNav";
 import VerifyEmail from "./VerifyEmail";
 
@@ -14,12 +11,12 @@ const isEmail = (value) => value.includes("@");
 const isNotEmpty = (value) => value.trim() !== "";
 
 const SignIn = () => {
-  // const history = useHistory();
-  // const authCtx = useContext(AuthContext);
-
   const [isLoading, setIsLoading] = useState(false);
   const [httpError, setHttpError] = useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [passwordType, setPasswordType] = useState("password");
+
+  const [passwordError, setPasswordError] = useState(false);
 
   const {
     value: firstNameValue,
@@ -65,6 +62,15 @@ const SignIn = () => {
     reset: resetPassword,
   } = useInput(isPassword);
 
+  const {
+    value: confirmPasswordValue,
+    isValid: confirmPasswordIsValid,
+    hasError: confirmPasswordHasError,
+    valueChangeHandler: confirmPasswordChangeHandler,
+    inputBlurHandler: confirmPasswordBlurHandler,
+    reset: resetConfirmPassword,
+  } = useInput(isPassword);
+
   let formIsValid = false;
 
   if (
@@ -72,10 +78,19 @@ const SignIn = () => {
     lastNameIsValid &&
     emailIsValid &&
     phoneNumberIsValid &&
-    passwordIsValid
+    passwordIsValid &&
+    confirmPasswordIsValid
   ) {
     formIsValid = true;
   }
+
+  const togglePasswordType = () => {
+    if (passwordType === "password") {
+      setPasswordType("text");
+      return;
+    }
+    setPasswordType("password");
+  };
 
   const toggleCloseNavigationHandler = () => {
     setIsLoggedIn(false);
@@ -83,6 +98,12 @@ const SignIn = () => {
 
   const submitHandler = (event) => {
     event.preventDefault();
+
+    if (passwordValue !== confirmPasswordValue) {
+      setPasswordError(true);
+
+      return;
+    }
 
     if (!formIsValid) {
       return;
@@ -101,100 +122,19 @@ const SignIn = () => {
       .then(function (response) {
         setIsLoading(false);
         setIsLoggedIn(true);
-        // history.replace("/dashboard");
       })
-      // .then((data) => {
-      //   const expirationTime = new Date(
-      //     new Date().getTime() + +data.expiresIn * 1000
-      //   );
-      //   authCtx.login(data.idToken, expirationTime.toISOString());
 
-      // })
       .catch(function (error) {
         setIsLoading(false);
         setHttpError(error.response.data.msg);
       });
 
-    // try {
-    //   const response = axios.post(
-    //     "https://foodblogafrika.herokuapp.com/api/v1/auth/register",
-    //     {
-    //       email: emailValue,
-    //       password: passwordValue,
-    //       phone: phoneNumberValue,
-    //       first_name: firstNameValue,
-    //       last_name: firstNameValue,
-    //     }
-    //   );
-
-    //   // history.replace("/dashboard");
-    // } catch (error) {
-    //   setIsLoading(false);
-
-    //   alert(error.response.data.msg);
-    // }
-    // setIsLoading(false);
-
-    // .then(function (response) {
-    //   setIsLoading(false);
-    //   if (response.ok) {
-    //     return response.json();
-    //   } else {
-    //     return response.json().then();
-    //   }
-    //   console.log(response);
-    // })
-    // .catch(function (error) {
-    //   console.log(error);
-    // });
-
-    // fetch(
-    //   "https://foodblogafrika.herokuapp.com/api/v1/auth/register",
-    //   // "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDXFa7ElAN-91B8g1G1Ebc3NtWHwxHj3gY",
-    //   {
-    //     method: "POST",
-    //     body: JSON.stringify({
-    //       email: emailValue,
-    //       password: passwordValue,
-    //       phone: phoneNumberValue,
-    //       first_name: firstNameValue,
-    //       last_name: firstNameValue,
-    //       // returnSecureToken: true,
-    //     }),
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //   }
-    // )
-    //   .then((res) => {
-    //     setIsLoading(false);
-    //     if (res.ok) {
-    //       return res.json();
-    //     } else {
-    //       return res.json().then((data) => {
-    //         let errorMessage = "Authentication failed!";
-    //         if (data && data.error && data.error.message) {
-    //           errorMessage = data.error.message;
-    //         }
-    //         throw new Error(errorMessage);
-    //       });
-    //     }
-    //   })
-    //   .then((data) => {
-    //     const expirationTime = new Date(
-    //       new Date().getTime() + +data.expiresIn * 1000
-    //     );
-    //     authCtx.login(data.idToken, expirationTime.toISOString());
-    //     history.replace("/dashboard");
-    //   })
-    //   .catch((err) => {
-    //     alert(err.message);
-    //   });
     resetFirstName();
     resetLastName();
     resetEmail();
     resetPhoneNumber();
     resetPassword();
+    resetConfirmPassword();
   };
 
   return (
@@ -217,6 +157,7 @@ const SignIn = () => {
               onChange={firstNameChangeHandler}
               onBlur={firstNameBlurHandler}
             />
+
             {firstNameHasError && (
               <p className={classes["error-text"]}>
                 Firstname must not be empty
@@ -240,7 +181,7 @@ const SignIn = () => {
           </div>
           <div className={classes["input__control"]}>
             <input
-              type="text"
+              type="number"
               className={classes[phoneNumberHasError ? "invalid" : "input"]}
               placeholder="Phone Number"
               value={phoneNumberValue}
@@ -266,18 +207,137 @@ const SignIn = () => {
             )}
           </div>
 
-          <div className={classes["input__control"]}>
+          <div
+            className={`${classes["input__control"]} ${classes["input__password"]}`}
+          >
             <input
               className={classes[passwordHasError ? "invalid" : "input"]}
-              type="password"
+              type={passwordType}
               placeholder="Password"
               value={passwordValue}
               onChange={passwordChangeHandler}
               onBlur={passwordBlurHandler}
+              span
             />
+
+            <i
+              className="absolute  z-20 px-6 pt-2 cursor-pointer"
+              onClick={togglePasswordType}
+            >
+              {passwordType === "password" ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                  />
+                </svg>
+              )}
+            </i>
+          </div>
+          <div>
             {passwordHasError && (
               <p className={classes["error-text"]}>
                 Password must be at least six letters
+              </p>
+            )}
+          </div>
+
+          <div
+            className={`${classes["input__control"]} ${classes["input__password"]}`}
+          >
+            <input
+              className={`${
+                classes[confirmPasswordHasError ? "invalid" : "input"]
+              } ${classes[passwordError ? "invalid" : "input"]}`}
+              type={passwordType}
+              placeholder="Comfirm Password"
+              value={confirmPasswordValue}
+              onChange={confirmPasswordChangeHandler}
+              onBlur={confirmPasswordBlurHandler}
+              span
+            />
+
+            <i
+              className="absolute  z-20 px-6 pt-2 cursor-pointer"
+              onClick={togglePasswordType}
+            >
+              {passwordType === "password" ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                  />
+                </svg>
+              )}
+            </i>
+          </div>
+
+          <div className="mb-4">
+            {confirmPasswordHasError && (
+              <p className={classes["error-text"]}>
+                Password must be at least six letters
+              </p>
+            )}
+
+            {passwordError && (
+              <p className="text-red-600 text-xs inline-block">
+                Password do not match
               </p>
             )}
           </div>
